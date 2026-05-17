2296bb45d4af37669c4927a89426592aead0ac3f
Kaue557 <kauelima557@gmail.com>
2026-05-17 20:48:20 -0300

    FINALIZADO 3.0 - NINGUEM MEXE MAIS!

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,14 @@
+/*
+========================================
+NOME                            RA
+
+Aline Vidal                     10721348
+João Vitor Fernandes Messias    10723552
+Kauê Lima Rodrigues Meneses     10410594
+Rayana Pimentel Marques Lopes   10435370
+========================================
+ */
+
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
